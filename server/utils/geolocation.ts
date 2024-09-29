159cc920370a4ccf7df095058354d589bcc29d16
s.nakamura <shotenba@gmail.com>
2024-09-29 16:23:11 +0900

    ♻️ tsdocの書き方に変更

--- server/utils/geolocation.ts
+++ server/utils/geolocation.ts
@@ -1,4 +1,7 @@
-// 位置情報を取得するメソッド
+/** 
+ * 現在位置の緯度経度を取得する
+ * @returns {Promise<{latitude: number, longitude: number}>} 緯度経度
+*/
 export const getCurrentLocation = (): Promise<{
   latitude: number;
   longitude: number;
